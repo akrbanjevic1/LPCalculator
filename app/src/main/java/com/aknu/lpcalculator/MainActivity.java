170d3cf0f590711ac2d49fe42bab7872ac326784
akrbanjevic1 <akrbanjevic1@luc.edu>
2019-06-21 19:24:51 -0500

    Finished the onClick listeners for all the buttons involved.
    Signed-off-by: akrbanjevic1 <akrbanjevic1@luc.edu>

--- app/src/main/java/com/aknu/lpcalculator/MainActivity.java
+++ app/src/main/java/com/aknu/lpcalculator/MainActivity.java
@@ -1,13 +1,50 @@
 package com.aknu.lpcalculator;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mLifePoints = 4000;
+    private TextView mShowLP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowLP = (TextView) findViewById(R.id.LP);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void DecreaseLP100(View view) {
+        mLifePoints -= 100;
+        if(mShowLP != null) {
+            mShowLP.setText(Integer.toString(mLifePoints));
+        }
+    }
+
+    public void IncreaseLP100(View view) {
+        mLifePoints += 100;
+        if(mShowLP != null) {
+            mShowLP.setText (Integer.toString(mLifePoints));
+        }
+    }
+
+    public void DecreaseLP25(View view) {
+        mLifePoints -= 25;
+        if(mShowLP != null) {
+            mShowLP.setText(Integer.toString(mLifePoints));
+        }
+    }
+
+    public void IncreaseLP25(View view) {
+        mLifePoints += 25;
+        if(mShowLP != null) {
+            mShowLP.setText(Integer.toString(mLifePoints));
+        }
     }
 }
